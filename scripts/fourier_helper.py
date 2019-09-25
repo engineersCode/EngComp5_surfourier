bf1630286656eab9342bc2817843bcbb4b1286e3
Naty Clementi <natyclementi@gmail.com>
2019-09-25 17:28:33 -0400

    progress on screipt and nb

--- scripts/fourier_helper.py
+++ scripts/fourier_helper.py
@@ -218,3 +218,129 @@ def update_figure_center(n, anim_dict, theta, display_fig=False):
         anim_dict['signals'][i].set_data(theta[:n + 1], elem.signal(theta[:n + 1]))
     if display_fig:
         display(anim_dict['fig'])
+
+# Functions needed for sum of waves plot
+
+
+def create_elements_displaced(freq, x_shift, y_shift):
+    """ It creates displaced elements to produce animation
+    for the sum of different waves and the respective circles based on
+    their frequency. The first circle is center while the following ones are 
+    center at the radius of the previous one and angle zero to start.
+
+    Arguments
+    ---------
+    freq: float or list. Frequency of desired waves.
+    x_shift: float, x-shift of the circle (x-coord of center)
+    y_shift: float, y-shift of the circle (y-coord of center)
+
+    Returns
+    -------
+    elems: list with the elements all center on same coordinates.
+    """
+
+    elements = list()
+    # Create first element (it'll be centered) 
+    elements.append(Element(f=freq[0], shift_x=x_shift, shift_y=y_shift))
+    # Create next elements center in the radius at angle 0
+    for i, f in enumerate(freq[1:]):
+        x, y = elements[i].circle.coordinates(0.0)
+        elements.append(Element(f=f, shift_x=x, shift_y=y))
+    
+    return elements
+
+
+def create_init_fig_displaced(elements, theta_arr, label_sum):
+    """ Creates initial figure needed for animation. In this case the 
+    figure is the one that has all the circles centered. 
+
+    Arguments
+    ---------
+    elements: list of objects, list of objects created with the function 
+    create_elements_center. 
+    theta: array, array of angles created with define_angles.
+
+    Returns
+    -------
+    Plot with initial figure needed to plot animation. 
+    """
+    pyplot.ioff() #to avoid displaying figure.
+
+    fig, ax = pyplot.subplots(figsize=(14.0, 6.0))
+    ax.grid()
+    ax.set_xlabel(r'$\theta$')
+    ax.axvline(0.0, color='black')
+    circles, rlines = [], []
+
+    trace_x = numpy.full_like(theta_arr, None)
+    trace_y = numpy.full_like(theta_arr, None)
+
+    for i, elem in enumerate(elements):
+        color = 'C' + str(i)
+        circles.append(ax.plot(*elem.circle.coordinates(), color=color, linewidth=2.0)[0])
+        rlines.append(ax.plot(*elem.rline(theta_arr[0]), color=color)[0])
+
+    trace_x[0] = elements[-1].rline(theta_arr[0])[0][1]
+    trace_y[0] = elements[-1].rline(theta_arr[0])[1][1]
+
+    trace_plot = ax.plot(trace_x, trace_y, color='k', linestyle='-')[0]
+
+    hline = ax.plot(*elements[-1].hline(theta_arr[0]), color=color, linestyle='--')[0]
+    signal = ax.plot(theta_arr[:1],
+                    numpy.sum(numpy.vstack([elem.signal(theta_arr[:1]) for elem in elements]), axis=0),
+                    label=label_sum, color=color, linewidth=2.0)[0]
+    ax.legend(ncol=len(elements), loc='lower center', prop={'size': 12})
+    ax.axis('scaled', adjustable='box')
+    ax.set_xlim(-5.0, 14.0)
+    ax.set_ylim(-2.0, 2.0);
+
+    return fig, ax, circles, rlines, trace_x, trace_y, trace_plot, hline, signal 
+
+
+def update_figure_displaced(n, anim_dict, theta, display_fig=False):
+    """Update the figure at a given step.
+
+    Parameters
+    ----------
+    n : integer
+        The step index.
+    anim_dict : dict, contains elements needed to create animation.
+    theta : numpy.ndarray of floats
+        All the angles.
+    display_fig : boolean (optional)
+        Set True to display the fig with ipywidgets.interactive;
+        default: False.
+
+    """
+    # Update the position of the radial line of the biggest circle.
+    anim_dict['rlines2'][0].set_data(*anim_dict['elems_dis'][0].rline(theta[n]))
+    # Update circle position and radial line for all elements except the biggest one.
+    for i in range(1, len(anim_dict['elems_dis'])):
+        # Use the outer extremity of the radial line as center of the new circle.
+        rline_pos = anim_dict['elems_dis'][i - 1].rline(theta[n])
+        xc, yc = rline_pos[0][1], rline_pos[1][1]
+        anim_dict['elems_dis'][i].circle = Circle(R=anim_dict['elems_dis'][i].circle.R, xc=xc, yc=yc)
+        # Update the circle on the axis.
+        anim_dict['circles2'][i].set_data(*anim_dict['elems_dis'][i].circle.coordinates())
+        # Update the radial line on the axis
+        anim_dict['rlines2'][i].set_data(*anim_dict['elems_dis'][i].rline(theta[n]))
+    # Update the horizontal line on the axis for the smallest circle.
+    anim_dict['hline2'].set_data(*anim_dict['elems_dis'][-1].hline(theta[n], xmax=0.0))
+    
+    rline_pos = anim_dict['elems_dis'][-1].rline(theta[n])
+    x, y = rline_pos[0][1], rline_pos[1][1]
+    anim_dict['trace_x'][n] = x
+    anim_dict['trace_y'][n] = y
+    
+    anim_dict['trace_x'][n+1:] = None
+    anim_dict['trace_y'][n+1:] = None
+    #print(trace_x[:3], trace_y[:3])
+    
+    anim_dict['trace_plot'].set_data(anim_dict['trace_x'], anim_dict['trace_y'])
+
+    # Compute and update the signal.
+    s = numpy.sum(numpy.vstack([elem.signal(theta[: n + 1]) for elem in anim_dict['elems_dis']]), axis=0)
+    anim_dict['signal2'].set_data(theta[:n + 1], s[::-1])
+        
+    if display_fig:
+        display(anim_dict['fig2'])
